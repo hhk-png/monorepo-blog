3a15eb1c4fa32512c48e85c3297493163b54e50c
hhk-png <fangluoabc@163.com>
2023-04-05 22:15:30 +0800

    init

---
+++ api/base.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+let Axios = axios.create({
+  baseURL: "/"
+})
+
+export default Axios
+
---
+++ api/user.js
@@ -0,0 +1,5 @@
+import Axios from "./base"
+
+
+export const getUser = (data) => Axios.get(data)
+
